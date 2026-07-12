8b5d7e693d9d8bb508c6e1f97f95b60d9bdc0e4b
Ayush Patel <ayushpatel241129@gmail.com>
2026-07-12 13:33:52 +1000

    Add Supabase configuration

---
+++ config.js
@@ -0,0 +1,7 @@
+const STOREFLOW_CONFIG = {
+    supabaseUrl:
+      "https://nrbtdevynbkfvpdcbzwj.supabase.co",
+  
+    supabasePublishableKey:
+      "sb_publishable_4Sktp6x9DhCQIWD2q9Y2mQ_9YaAgYqP"
+  };
